77a11a8e78491cc864bddc840c7c023aeb09c6ae
Himanshu kumar <99878857+himan1009@users.noreply.github.com>
2024-12-19 22:22:04 +0530

    Time: 0 ms (100%), Space: 7.8 MB (24.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int func(int n, vector<int> &dp){
+// space optimisation
+    int fib(int n) {
         if(n<=1)
         return n;
-        
-        if(dp[n]!=-1)
-        return dp[n];
-
-        return dp[n]=func(n-1, dp)+func(n-2, dp);
-    }
-    int fib(int n) {
-        vector<int> dp(n+1, -1);
-        return func(n, dp);
+        int prev2=0;
+        int prev1=1;
+        for(int i=2;i<=n;i++){
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
     }
 };
